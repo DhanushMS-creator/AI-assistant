9a28fc1f3e22f2d10a97067a709f1e0d2722f24e
Dhanush Kumar M <dhanush@Dhanushs-MacBook-Air.local>
2026-01-18 21:18:36 +0530

    update

--- client-react/src/pages/Login.jsx
+++ client-react/src/pages/Login.jsx
@@ -47,7 +47,7 @@ const Login = () => {
 
             <div style={{ marginTop: '30px' }}>
                 <div id="g_id_onload"
-                     data-client_id="YOUR_GOOGLE_CLIENT_ID_HERE"
+                     data-client_id="85725976781-jikri2iuo1odlqasejb7ffll9ldkvdc6.apps.googleusercontent.com"
                      data-callback="handleCredentialResponse"
                      data-auto_prompt="false">
                 </div>
